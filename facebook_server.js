80332c1bfe55f84579871832058c4c18c935828b
Nick McIntosh <nick@nickmcintosh>
2016-02-11 15:02:38 +1100

    profilePictureURL attribute added to user if available, increased picture size to FB minimum

--- facebook_server.js
+++ facebook_server.js
@@ -1,56 +1,54 @@
-Accounts.registerLoginHandler(function(loginRequest) {
-  if(!loginRequest.cordova) {
+/* eslint-disable no-param-reassign */
+/* jscs:disable requireCamelCaseOrUpperCaseIdentifiers */
+Accounts.registerLoginHandler(loginRequest => {
+  if (!loginRequest.cordova) {
     return undefined;
   }
 
   loginRequest = loginRequest.authResponse;
-
-  var whitelisted = ['id', 'email', 'name', 'first_name',
-      'last_name', 'link', 'gender', 'locale', 'age_range'];
-
-  var identity = getIdentity(loginRequest.accessToken, whitelisted);
-
-  var profilePicture = getProfilePicture(loginRequest.accessToken);
-
-  var serviceData = {
+  const whitelisted = [
+    'id', 'email', 'name', 'first_name', 'last_name', 'link', 'gender', 'locale', 'age_range',
+  ];
+  const identity = getIdentity(loginRequest.accessToken, whitelisted);
+  const profilePicture = getProfilePicture(loginRequest.accessToken);
+  const serviceData = {
     accessToken: loginRequest.accessToken,
-    expiresAt: (+new Date) + (1000 * loginRequest.expiresIn)
+    expiresAt: (+new Date) + (1000 * loginRequest.expiresIn),
   };
+  const fields = _.pick(identity, whitelisted);
+  const options = { profile: { } };
+  const profileFields = _.pick(identity, Meteor.settings.public.facebook.profileFields);
 
-
-  var fields = _.pick(identity, whitelisted);
   _.extend(serviceData, fields);
-
-  var options = {profile: {}};
-  var profileFields = _.pick(identity, Meteor.settings.public.facebook.profileFields);
   _.extend(options.profile, profileFields);
+  options.profile.profilePictureURL = profilePicture;
+  serviceData.profilePictureURL = profilePicture;
 
-  options.profile.avatar = profilePicture;
-
-  return Accounts.updateOrCreateUserFromExternalService("facebook", serviceData, options);
-
+  return Accounts.updateOrCreateUserFromExternalService('facebook', serviceData, options);
 });
 
-var getIdentity = function (accessToken, fields) {
+function getIdentity(accessToken, fields) {
   try {
-    return HTTP.get("https://graph.facebook.com/me", {
+    return HTTP.get('https://graph.facebook.com/me', {
       params: {
         access_token: accessToken,
-        fields: fields
-      }
+        fields,
+      },
     }).data;
   } catch (err) {
-    throw _.extend(new Error("Failed to fetch identity from Facebook. " + err.message),
-                   {response: err.response});
+    throw _.extend(new Error(`Failed to fetch identity from Facebook: ${err.message}`),
+                   { response: err.response });
   }
-};
+}
 
-var getProfilePicture = function (accessToken) {
+function getProfilePicture(accessToken) {
   try {
-    return HTTP.get("https://graph.facebook.com/v2.0/me/picture/?redirect=false", {
-      params: {access_token: accessToken}}).data.data.url;
+    // Minimum FB profile pic size is 180x180px
+    return HTTP.get('https://graph.facebook.com/v2.0/me/picture/?redirect=false&height=180&width=180', {
+      params: { access_token: accessToken },
+    }).data.data.url;
   } catch (err) {
-    throw _.extend(new Error("Failed to fetch identity from Facebook. " + err.message),
-                   {response: err.response});
+    throw _.extend(new Error(`Failed to fetch identity from Facebook: ${err.message}`),
+                   { response: err.response });
   }
-};
+}
--- package.js
+++ package.js
@@ -1,23 +1,30 @@
+/* eslint-disable prefer-arrow-callback */
+/* jscs:disable requireCamelCaseOrUpperCaseIdentifiers */
 Package.describe({
-  summary: "Login service for Facebook (2.4) accounts (works with cordova)",
-  version: "0.0.3",
-  git: "https://github.com/btafel/accounts-facebook.git",
-  author: "Nick McIntosh",
-  name: "heysailor:accounts-facebook-cordova",
+  summary: 'Login service for Facebook (2.4) accounts (works with cordova)',
+  version: '0.0.3',
+  git: 'https://github.com/btafel/accounts-facebook.git',
+  author: 'Nick McIntosh',
+  name: 'heysailor:accounts-facebook-cordova',
 });
 
-Package.on_use(function(api) {
+Package.onUse(function config(api) {
   api.versionsFrom('1.2.0.1');
-  api.use('accounts-base', ['client', 'server']);
-  // Export Accounts (etc) to packages using this one.
-  api.imply('accounts-base', ['client', 'server']);
-  api.use('accounts-oauth', ['client', 'server']);
-  api.use('facebook', ['client', 'server']);
+  api.use([
+    'ecmascript',
+    'accounts-base',
+    'accounts-oauth',
+    'facebook',
+    'service-configuration',
+    'http',
+    'underscore',
+  ], ['client', 'server']);
 
-  api.use('service-configuration', ['client', 'server']);
-  api.use('http', ['server']);
-  api.use('underscore', ['server']);
+  // Export Accounts (etc) to packages using this one.
+  api.imply([
+    'accounts-base',
+  ], ['client', 'server']);
 
   api.add_files('facebook_server.js', 'server');
-  api.add_files("facebook.js");
+  api.add_files('facebook.js');
 });
